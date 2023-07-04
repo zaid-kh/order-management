e536e61a554b366f92ef927094f0c9b4ce94fcb1
zaid-kh <z.kh2800@gmail.com>
2023-07-04 17:31:49 +0300

    fix: fixed Stock API finding stocks by product ID

--- src/main/java/edu/bzu/ordermanagement/repository/StockRepository.java
+++ src/main/java/edu/bzu/ordermanagement/repository/StockRepository.java
@@ -2,6 +2,14 @@ package edu.bzu.ordermanagement.repository;
 
 import edu.bzu.ordermanagement.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    // Custom query to find stocks by productId
+    @Query("SELECT s FROM Stock s WHERE s.product.id = :productId")
+    List<Stock> findByProductId(@Param("productId") Long productId);
+
 }
--- src/main/java/edu/bzu/ordermanagement/service/impl/StockServiceImpl.java
+++ src/main/java/edu/bzu/ordermanagement/service/impl/StockServiceImpl.java
@@ -1,12 +1,10 @@
 package edu.bzu.ordermanagement.service.impl;
 
-import edu.bzu.ordermanagement.entity.Product;
 import edu.bzu.ordermanagement.entity.Stock;
 import edu.bzu.ordermanagement.repository.StockRepository;
 import edu.bzu.ordermanagement.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -48,19 +46,9 @@ public class StockServiceImpl implements StockService {
         return stockRepository.findById(id);
     }
 
-    // TODO: Fix this method {returns empty list}
     @Override
     public List<Stock> getStocksByProductId(Long productId) {
-        Stock stockExample = new Stock();
-        Product product = new Product();
-        product.setId(productId);
-        stockExample.setProduct(product);
-        // matching criteria: exact match for product id only
-        ExampleMatcher matcher = ExampleMatcher.matching()
-                .withIgnorePaths("id", "updatedAt", "quantity")
-                .withIgnoreNullValues()
-                .withMatcher("product", ExampleMatcher.GenericPropertyMatcher::exact);
-        Example<Stock> example = Example.of(stockExample, matcher);
-        return stockRepository.findAll(example);
+
+        return stockRepository.findByProductId(productId);
     }
 }
